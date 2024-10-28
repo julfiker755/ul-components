e5633c3fae1f34ae9bb3aed6ea6bf0da7c69dfa9
julfiker755 <julfiker755.bd@gmail.com>
2024-10-29 02:15:19 +0600

    button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 
 
 function Rootpage() {
   return (
-    <div>Rootpage</div>
+    <div>
+      <Link href="/components/button">
+      <button className='bg-[green] py-2 px-4 text-white'>components</button>
+      </Link>
+    </div>
   )
 }
 
